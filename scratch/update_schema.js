44425672d00459c53df36301e40e4374fd9f058d
Sharma Dhruv <2004dhrusharma@gmail.com>
2026-06-26 10:33:10 +0530

    Remove untracked data folders from git

--- scratch/update_schema.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('backend/prisma/schema.prisma', 'utf8');
-content = content.replace(/provider = "mysql"/g, 'provider = "sqlite"');
-content = content.replace(/@db\.Text/g, '');
-fs.writeFileSync('backend/prisma/schema.prisma', content);
-console.log('Schema updated to SQLite');
